31a4663403850ba823a2eb34ef2abb33d3717386
Lauren Rondenet <laurenrondenet@laurens-mbp.attlocal.net>
2021-04-14 20:15:32 -0700

    database config

--- travel_buddies/settings.py
+++ travel_buddies/settings.py
@@ -74,9 +74,12 @@ WSGI_APPLICATION = 'travel_buddies.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travel_buddies',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost'
     }
 }
 
