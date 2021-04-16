59e37299af2d1258770e7a697f3fd2ea064460c7
Lauren Rondenet <laurenrondenet@laurens-mbp.attlocal.net>
2021-04-16 15:56:21 -0700

    settings

--- travel_buddies/settings.py
+++ travel_buddies/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'travel_buddies.travel',
-    'travel_buddies.accounts'
+    'travel_buddies.accounts',
+    'travel_buddies.suggestions'
 
 ]
 
