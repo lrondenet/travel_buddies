a2272cb259e0a416b46ffed3abf8927b7341b4b6
lrondenet <lrondenet1@mail.csuchico.edu>
2021-04-20 11:11:59 -0700

    testing suggestions

--- travel_buddies/suggestions/tests.py
+++ travel_buddies/suggestions/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from travel_buddies.suggestions.serializers import SuggestionsSerializer
+import logging
 
-# Create your tests here.
+logger = logging.getLogger(__name__)
+
+class UserSerializerCase(TestCase):
+    def test_valid_serializer(self):
+        serializer = SuggestionsSerializer(data={'start_date':'04/20/21','end_date':'04/24/21','vote':2})
+        logger.error(repr(serializer))
+        assert serializer.is_valid()
