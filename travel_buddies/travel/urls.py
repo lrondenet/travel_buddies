f157395021c44b19d8fdc7c61ae8763bcf48db32
lrondenet <lrondenet1@mail.csuchico.edu>
2021-04-23 23:10:03 -0700

    update views.py

--- travel_buddies/travel/urls.py
+++ travel_buddies/travel/urls.py
@@ -3,8 +3,8 @@ from travel_buddies.travel import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('trips/', views.trip_list),
-    path('trips/<int:pk>/', views.trip_detail),
+    path('trips/', views.TripList.as_view()),
+    path('trips/<int:pk>/', views.TripDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
