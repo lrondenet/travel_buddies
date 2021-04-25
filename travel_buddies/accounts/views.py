99d81f23d66f080fdf18b8c5791e2bf6eba331c6
lrondenet <lrondenet1@mail.csuchico.edu>
2021-04-25 11:21:19 -0700

    user views

--- travel_buddies/accounts/views.py
+++ travel_buddies/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.contrib.auth.models import User
+from travel_buddies.accounts.models import UserProfile
+from travel_buddies.accounts.serializers import UserSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class UserList(generics.ListAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserSerializer
+
+
+
+# should I add AuthUser too?
--- travel_buddies/urls.py
+++ travel_buddies/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('travel_buddies.travel.urls')),
+    path('', include('travel_buddies.accounts.urls'))
 ]
