248416eb163e9d0d3c8a8c9cfdf1eed86ca7cecd
lrondenet <lrondenet1@mail.csuchico.edu>
2021-04-25 21:28:06 -0700

    redoing user views

--- travel_buddies/accounts/views.py
+++ travel_buddies/accounts/views.py
@@ -1,18 +1,20 @@
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from travel_buddies.accounts.models import UserProfile
 from travel_buddies.accounts.serializers import UserSerializer
 from rest_framework import generics
 
 
-class UserList(generics.ListAPIView):
+class UserList(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserSerializer
 
 
 
-# should I add AuthUser too?
+# Todo;
+# need to change user edit to not pass in user id
+
