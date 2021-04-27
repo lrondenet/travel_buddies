63b662edd5ae693695fb1ef576092789ff0b17dd
lrondenet <lrondenet1@mail.csuchico.edu>
2021-04-26 20:15:16 -0700

    uncommenting test-need to merage main

--- travel_buddies/accounts/tests.py
+++ travel_buddies/accounts/tests.py
@@ -5,10 +5,10 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-# class UserSerializerCase(TestCase):
-#     def setup(self):
-#         lauren = User.objects.get(id=1)
-#     def test_valid_serializer(self):
-#         serializer = UserSerializer(data={'user':'lauren', 'address1':'1234 broadway', 'zipcode':94501, 'city':'Oakland', 'phone':'3452342345'})
-#         logger.error(repr(serializer))
-#         assert serializer.is_valid()
+class UserSerializerCase(TestCase):
+    def setup(self):
+        lauren = User.objects.get(id=1)
+    def test_valid_serializer(self):
+        serializer = UserSerializer(data={'user':'lauren', 'address1':'1234 broadway', 'zipcode':94501, 'city':'Oakland', 'phone':'3452342345'})
+        logger.error(repr(serializer))
+        assert serializer.is_valid()
