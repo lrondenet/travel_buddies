65d27fdc90d8dfedc5cc3a1fa1ef542e4058c411
lrondenet <lrondenet1@mail.csuchico.edu>
2021-04-28 12:04:10 -0700

    Authentication & Permissions

--- travel_buddies/suggestions/serializers.py
+++ travel_buddies/suggestions/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import Suggestions
 
 class SuggestionsSerializer(serializers.ModelSerializer):
+
+    user = serializers.ReadOnlyField(source='user_id')
+
     class Meta:
         model = Suggestions
-        fields = '__all__'
+        fields = ['user', 'trip_plan', 'start_date', 'end_date', 'vote']
 
--- travel_buddies/suggestions/views.py
+++ travel_buddies/suggestions/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import render
 from travel_buddies.suggestions.models import Suggestions
 from travel_buddies.suggestions.serializers import SuggestionsSerializer
 from rest_framework import generics
-
+from rest_framework import permissions
+from travel_buddies.accounts.models import UserProfile
 
 
 class SuggestionsList(generics.ListCreateAPIView):
     queryset = Suggestions.objects.all()
     serializer_class = SuggestionsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        auth_id = self.request.user.id
+        user_profile = UserProfile.objects.get(user_id=auth_id)
+        print(repr(user_profile))
+        # serializer.save(user=self.request.user_id)
+
 
 
 class SuggestionsDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Suggestions.objects.all()
     serializer_class = SuggestionsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
