80dd55efd5201a563b367400745b73ff773a991a
lrondenet <lrondenet1@mail.csuchico.edu>
2021-04-30 14:50:53 -0700

    fake accounts

---
+++ utils/faker/accounts.py
@@ -0,0 +1,11 @@
+from faker import Faker
+from django.contrib.auth.models import User
+from travel_buddies.accounts.models import UserProfile
+
+
+fake = Faker()
+
+for i in range(100):
+    temp_profile = fake.simple_profile()
+    user = User.objects.create_user(username=temp_profile['username'], email=temp_profile['mail'], password='lauren')
+    UserProfile.objects.create(user=user, address1=temp_profile['address'], zipcode=fake.postcode(), city=fake.city(), country=fake.country(), phone=fake.phone_number())
