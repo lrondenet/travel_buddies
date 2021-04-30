60efe9800b0349879a4c6c3bec226d6fd4cf7147
lrondenet <lrondenet1@mail.csuchico.edu>
2021-04-30 15:05:54 -0700

    fixed validation for suggestions

--- travel_buddies/suggestions/serializers.py
+++ travel_buddies/suggestions/serializers.py
@@ -5,6 +5,13 @@ class SuggestionsSerializer(serializers.ModelSerializer):
 
     user = serializers.ReadOnlyField(source='user_id')
 
+    def validate(self, data):
+        if data['start_date'] > data['end_date']:
+            raise serializers.ValidationError("end_date must be greater than start_date")
+        if data['vote'] < 0:
+            raise serializers.ValidationError("vote must be a positive number")
+        return data
+
     # Todo
     # make start_date not putable or always equal
 
@@ -12,3 +19,4 @@ class SuggestionsSerializer(serializers.ModelSerializer):
         model = Suggestions
         fields = ['user', 'trip_plan', 'start_date', 'end_date', 'vote']
 
+    
