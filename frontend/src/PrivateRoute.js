96d2ccd96f1330aa6f3b8ad61182ac73445b3dbe
lrondenet <lrondenet1@mail.csuchico.edu>
2021-05-14 11:23:03 -0700

    fixing PrivateRoute

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -1,52 +1,29 @@
 import React, { useState, useEffect } from 'react'
-import { Redirect, Route } from 'react-router-dom'
+import {Route, useHistory } from 'react-router-dom'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const [isLoggedIn, setLoggedIn] = useState(false)
+  const history = useHistory();
+
   // Add your own authentication on the below line.
-  
-  //   const isLoggedIn = token !== null;
-
   useEffect(() => {
-    const token = localStorage.getItem('token')
-    console.log('LAUREN')
-    let mounted = true
-    fetch('http://localhost:8000/trips/', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Token ' + token,
-      },
-    }).then((resp) => {
-      console.log(isLoggedIn)
-      console.log(resp)
-      console.log(mounted)
-      if (resp.ok) {
-        if (mounted) {
-          setLoggedIn(true)
-          console.log('setting state', isLoggedIn)
+      async function validateToken() {
+        const token = localStorage.getItem('token') 
+        const response = await fetch('http://localhost:8000/trips/', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Token ' + token,
+            },
+        });
+        if (response.ok) {
+          setLoggedIn(true);
+          return
         }
-      }
-    })
-    // return () => {
-    //   mounted = false
-    // }
-  }, [isLoggedIn])
-
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        isLoggedIn ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: '/login', state: { from: props.location } }}
-          />
-        )
-      }
-    />
-  )
+        history.push('/login')
+    }
+    validateToken();
+  }, [isLoggedIn, history]);
+  return <Route {...rest} render={(props) => <Component {...props} />} />
 }
-
 export default PrivateRoute
