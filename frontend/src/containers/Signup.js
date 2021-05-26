336ede18ebf967f59ec33b3ae06f5fe5a16070c3
lrondenet <lrondenet1@mail.csuchico.edu>
2021-05-26 14:32:48 -0700

    signup

--- frontend/src/containers/Signup.js
+++ frontend/src/containers/Signup.js
@@ -1,108 +1,40 @@
-import React from 'react'
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import TextField from '@material-ui/core/TextField'
-import Link from '@material-ui/core/Link'
-import Grid from '@material-ui/core/Grid'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import Typography from '@material-ui/core/Typography'
-import { withStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
-import Alert from '@material-ui/lab/Alert';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 
-const styles = (theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', 
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-});
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+}));
+  
+export default function SignUp() {
+    const classes = useStyles();
 
-
-class Signup extends React.Component {
-  state = {
-    firstName: '',
-    lastName: '',
-    username: '',
-    email: '',
-    address1: '',
-    address2: '',
-    city: '',
-    country: '',
-    zipcode: '',
-    phone: '',
-    password: '',
-    isError: false,
-    isSuccess: false
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault()
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(
-        {
-          user: {
-            first_name: this.state.firstName,
-            last_name: this.state.lastName,
-            username: this.state.username,
-            email: this.state.email,
-            password: this.state.password,
-          },
-          address1: this.state.address1,
-          address2: this.state.address2,
-          city: this.state.city,
-          country: this.state.country,
-          zipcode: this.state.zipcode,
-          phone: this.state.phone,
-        },
-        (key, value) => {
-          if (value !== '') return value
-        }
-      ),
-    }
-    fetch('http://localhost:8000/users/', requestOptions).then((response) => {
-      if (!response.ok) {
-        this.setState({
-          isError: true
-        }) 
-      }
-      if (response.ok) {
-        this.setState({
-          isSuccess: true
-        })
-      }
-      response.json()
-    })
-  }
-
-  handleInputChange = (event) => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-
-    this.setState({
-      [name]: value,
-    })
-  }
-
-  render() {
-    const { classes } = this.props
+  
     return (
       <Container component="main" maxWidth="xs">
         <CssBaseline />
@@ -113,25 +45,7 @@ class Signup extends React.Component {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          {this.state.isError ? (
-            <Alert severity="error">
-              There is an error!
-            </Alert>
-          ) : (
-            <div></div>
-          )}
-          {this.state.isSuccess ? (
-            <Alert severity="success">
-              You created an account!
-            </Alert>
-          ) : (
-            <div></div>
-          )}
-          <form
-            className={classes.form}
-            noValidate
-            onSubmit={this.handleSubmit}
-          >
+          <form className={classes.form} noValidate>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -143,7 +57,8 @@ class Signup extends React.Component {
                   id="firstName"
                   label="First Name"
                   autoFocus
-                  onChange={this.handleInputChange}
+                //   onChange={handleChange('first_name')}
+                //   defaultValue={values.first_name}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -155,7 +70,6 @@ class Signup extends React.Component {
                   label="Last Name"
                   name="lastName"
                   autoComplete="lname"
-                  onChange={this.handleInputChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -167,9 +81,8 @@ class Signup extends React.Component {
                   label="Username"
                   name="username"
                   autoComplete="username"
-                  onChange={this.handleInputChange}
                 />
-              </Grid>
+                </Grid>
               <Grid item xs={12}>
                 <TextField
                   variant="outlined"
@@ -179,7 +92,6 @@ class Signup extends React.Component {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
-                  onChange={this.handleInputChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -191,7 +103,6 @@ class Signup extends React.Component {
                   id="address1"
                   label="Address1"
                   autoFocus
-                  onChange={this.handleInputChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -203,7 +114,6 @@ class Signup extends React.Component {
                   id="address2"
                   label="Address2"
                   autoFocus
-                  onChange={this.handleInputChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -215,7 +125,6 @@ class Signup extends React.Component {
                   id="city"
                   label="City"
                   autoFocus
-                  onChange={this.handleInputChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -227,7 +136,6 @@ class Signup extends React.Component {
                   id="country"
                   label="Country"
                   autoFocus
-                  onChange={this.handleInputChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -239,7 +147,6 @@ class Signup extends React.Component {
                   id="zipcode"
                   label="Zipcode"
                   autoFocus
-                  onChange={this.handleInputChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -251,7 +158,6 @@ class Signup extends React.Component {
                   id="phone"
                   label="Phone"
                   autoFocus
-                  onChange={this.handleInputChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -264,7 +170,6 @@ class Signup extends React.Component {
                   type="password"
                   id="password"
                   autoComplete="current-password"
-                  onChange={this.handleInputChange}
                 />
               </Grid>
             </Grid>
@@ -287,7 +192,5 @@ class Signup extends React.Component {
           </form>
         </div>
       </Container>
-    )
-  }
+    );
 }
-export default withStyles(styles)(Signup)
